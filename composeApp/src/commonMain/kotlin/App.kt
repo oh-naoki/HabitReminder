a16a3ff7963b1672e73f59e8ee613d5cdbcb3a4c
oh-naoki <oh.naoki0819@gmail.com>
2024-06-09 16:12:32 +0900

    initial commit

---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,11 @@
+import androidx.compose.runtime.Composable
+import component.HabitReminderTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    HabitReminderTheme {
+
+    }
+}
